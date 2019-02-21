b9905ca56f5dbfab7e86a77f3702b7dcde67c5be
kuzukawakonayuki <konayuki0530@outlook.jp>
2019-02-22 04:17:38 +0900

    20190222-03:00:00-start

--- bot.py
+++ bot.py
@@ -66,7 +66,7 @@ async def on_ready():
     print('{0.user}'.format(client))
     print(client.user.id)
     print('------------------------')
-    next_send = "2019-02-22 01:00:00"
+    next_send = "2019-02-22 03:00:00"
     next_send = datetime.datetime.strptime(next_send, '%Y-%m-%d %H:%M:%S')
     asyncio.ensure_future(stage_send(next_send))
     
